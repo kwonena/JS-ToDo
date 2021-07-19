0dcfd2b82db4ee22b39339ffb1275f2f64829b58
KwonEna <eeena214@gmail.com>
2021-07-19 23:02:08 +0900

    no message

--- todo.js
+++ todo.js
@@ -1,14 +1,10 @@
 function pressEnter() {
     if(window.event.keyCode == 13) {
-        //const listText = document.getElementsByClassName("list-text")[0];
         const text = document.getElementById("list-title").value;
         let div = document.createElement('div');
-        div.innerHTML = `<label><input type="checkbox" name="check" class="listText"/>${text}</label>`;
+        div.innerHTML = `<label><input type="checkbox" name="check" class="listCheck"/><span class="listText">${text}</span></label>`;
         document.getElementById('todo-list').appendChild(div);
         
         alert("Okay!");
-        
     }
 }
-
-// listText가 checkbox 자체에 걸려서 css 적용시 문구가 아닌 checkbox에 적용됨
