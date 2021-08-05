2302aa529f97608d7fd39d8cc52233af51c0b9d9
KwonEna <eeena214@gmail.com>
2021-08-05 22:34:07 +0900

    Add : 체크박스 전체선택, 전체해제 코드 구현

--- todo.js
+++ todo.js
@@ -2,12 +2,26 @@ function pressEnter() {
     if(window.event.keyCode == 13) {
         const text = document.getElementById("list-title").value;
         let div = document.createElement('div');
-        div.innerHTML = `<label><input type="checkbox" name="check" class="listCheck"/><span class="listText">${text}</span></label>`;
+        div.innerHTML = 
+        `<label for="listCheck">
+            <input type="checkbox" name="check" class="listCheck"/>
+            <span class="listText">${text}</span>
+        </label>`;
         document.getElementById('todo-list').appendChild(div);
         
         alert("Okay!");
 
     }
+}
+
+// 체크박스 전체 선택, 전체 해제
+function selectAll(selectAll)  {
+    const checkboxes 
+       = document.querySelectorAll('input[type="checkbox"]');
+    
+    checkboxes.forEach((checkbox) => {
+      checkbox.checked = selectAll.checked
+    })
 }
 
 // checkbox 클릭시 listText에 줄긋기 이벤트
